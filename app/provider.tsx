ce077d4c309b66852d1a6dd09f545639e2c2f09d
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2026-01-27 19:23:15 +0600

    Creating endpoint of create new user

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useUser } from "@clerk/nextjs";
 
 function Provider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  const { user } = useUser();
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
